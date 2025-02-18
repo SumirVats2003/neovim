d5b7a13be1a1c16c14a68b0b22313133c083365d
Sumir Vats <sumirvats@gmail.com>
2025-02-18 04:32:34 +0000

    add undofile options and clean options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -1,12 +1,21 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 5
 vim.opt.showmode = false
 vim.opt.cursorline = true
+
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.termguicolors = true
+
 vim.g.mapleader = " "
 vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 250
-vim.opt.scrolloff = 5
+
+vim.opt.updatetime = 50
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
