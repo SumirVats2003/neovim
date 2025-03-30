ee95ae0c75d5fefcba47c80d1dd65ea8f75c51fe
Sumir Vats <sumirvats@gmail.com>
2025-03-30 03:51:48 +0000

    edit git branch icon

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -64,7 +64,8 @@ return {
       },
       sections = {
         lualine_a = { mode },
-        lualine_b = { { 'branch', icons_enabled = true, icon = '' } },
+        -- lualine_b = { { 'branch', icons_enabled = true, icon = '' } },
+        lualine_b = { { 'branch', icons_enabled = true, icon = ' ' } },
         lualine_c = { filename },
         lualine_x = { diagnostics, diff, { 'encoding', cond = hide_in_width }, { 'filetype', cond = hide_in_width } },
         lualine_y = { 'progress' },
