1460818d2af83ea8b1dec25bfc84b28c3f32ba19
Sumir Vats <sumirvats@gmail.com>
2025-03-31 15:49:28 +0000

    remove duplicate keybiniding for join

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,8 +24,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- thanks to ThePrimeagen
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 
 -- TJ the God
 vim.keymap.set('n', '<leader>z', ":Floaterminal<cr>")
