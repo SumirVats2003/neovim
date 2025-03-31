d7e71337c9270d8044139f79d59fdf3fdea98400
Sumir Vats <sumirvats@gmail.com>
2025-03-31 14:38:20 +0000

    added heirline

---
+++ lua/plugins/ui/heirline.lua
@@ -0,0 +1,49 @@
+return {
+  "rebelot/heirline.nvim",
+  dependencies = {
+    "zeioth/heirline-components.nvim",
+    opts = {
+      icons = {
+        GitBranch = " "
+      },
+    }
+  },
+  event = "VeryLazy",
+  opts = function()
+    local lib = require "heirline-components.all"
+    return {
+      opts = {
+        disable_winbar_cb = function(args)
+          local is_disabled = not require("heirline-components.buffer").is_valid(args.buf) or
+              lib.condition.buffer_matches({
+                -- buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
+                filetype = { "NvimTree", "neo%-tree", "dashboard", "Outline", "aerial" },
+              }, args.buf)
+          return is_disabled
+        end,
+      },
+      statusline = {
+        hl = { fg = "fg", bg = "bg" },
+        lib.component.mode({ mode_text = {} }),
+        lib.component.git_branch(),
+        lib.component.file_info({ filetype = false, filename = {}, file_modified = {} }),
+        lib.component.git_diff(),
+        lib.component.diagnostics(),
+        lib.component.fill(),
+        lib.component.cmd_info(),
+        lib.component.fill(),
+        lib.component.lsp(),
+        lib.component.compiler_state(),
+        lib.component.nav(),
+        lib.component.mode { surround = { separator = "right" } },
+      },
+    }
+  end,
+  config = function(_, opts)
+    local heirline = require("heirline")
+    local heirline_components = require "heirline-components.all"
+    heirline_components.init.subscribe_to_events()
+    heirline.load_colors(heirline_components.hl.get_colors())
+    heirline.setup(opts)
+  end,
+}
--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -255,100 +255,101 @@
 --   },
 -- }
 
-return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    local mode = {
-      'mode',
-      fmt = function(str)
-        return ' ' .. str
-        -- return '  ' .. str:sub(1, 1) -- displays only the first character of the mode
-      end,
-    }
-
-    local hide_in_width = function()
-      return vim.fn.winwidth(0) > 100
-    end
-
-    local filename = {
-      'filename',
-      path = 0,            -- 0 = just filename, 1 = relative path, 2 = absolute path
-      file_status = false, -- displays file status (readonly status, modified status)
-    }
-
-    local diagnostics = {
-      'diagnostics',
-      sources = { 'nvim_diagnostic' },
-      sections = { 'error', 'warn' },
-      symbols = { error = ' ', warn = ' ', info = ' ', hint = '󰛨 ' },
-      colored = false,
-      update_in_insert = false,
-      always_visible = false,
-      cond = hide_in_width,
-    }
-
-    local diff = {
-      'diff',
-      colored = true,
-      symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
-      cond = hide_in_width,
-    }
-
-    require('lualine').setup {
-      options = {
-        globalstatus = true,
-        theme = 'tokyonight',
-        icons_enabled = true,
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        -- component_separators = { left = '', right = '' },
-        -- section_separators = { left = '', right = '' },
-        -- component_separators = { left = '', right = '' },
-        -- section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-          statusline = {},
-          winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        always_show_tabline = true,
-        refresh = {
-          statusline = 100,
-          tabline = 100,
-          winbar = 100,
-        }
-      },
-      sections = {
-        lualine_a = { mode },
-        -- lualine_b = { { 'branch', icons_enabled = true, icon = '' } },
-        lualine_b = { { 'branch', icons_enabled = true, icon = ' ' } },
-        lualine_c = {
-          {
-            'filename',
-            symbols = {
-              modified = '',
-            }
-          }
-        },
-        lualine_x = { diagnostics, diff, { 'encoding', cond = hide_in_width }, { 'filetype', cond = hide_in_width } },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-      },
-      tabline = {
-        -- extensions = { 'vim-fugitive' },
-      },
-      winbar = {},
-      inactive_winbar = {},
-      extensions = {}
-    }
-  end
-}
+return {}
+-- return {
+--   'nvim-lualine/lualine.nvim',
+--   dependencies = { 'nvim-tree/nvim-web-devicons' },
+--   config = function()
+--     local mode = {
+--       'mode',
+--       fmt = function(str)
+--         return ' ' .. str
+--         -- return '  ' .. str:sub(1, 1) -- displays only the first character of the mode
+--       end,
+--     }
+--
+--     local hide_in_width = function()
+--       return vim.fn.winwidth(0) > 100
+--     end
+--
+--     local filename = {
+--       'filename',
+--       path = 0,            -- 0 = just filename, 1 = relative path, 2 = absolute path
+--       file_status = false, -- displays file status (readonly status, modified status)
+--     }
+--
+--     local diagnostics = {
+--       'diagnostics',
+--       sources = { 'nvim_diagnostic' },
+--       sections = { 'error', 'warn' },
+--       symbols = { error = ' ', warn = ' ', info = ' ', hint = '󰛨 ' },
+--       colored = false,
+--       update_in_insert = false,
+--       always_visible = false,
+--       cond = hide_in_width,
+--     }
+--
+--     local diff = {
+--       'diff',
+--       colored = true,
+--       symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
+--       cond = hide_in_width,
+--     }
+--
+--     require('lualine').setup {
+--       options = {
+--         globalstatus = true,
+--         theme = 'tokyonight',
+--         icons_enabled = true,
+--         component_separators = { left = '', right = '' },
+--         section_separators = { left = '', right = '' },
+--         -- component_separators = { left = '', right = '' },
+--         -- section_separators = { left = '', right = '' },
+--         -- component_separators = { left = '', right = '' },
+--         -- section_separators = { left = '', right = '' },
+--         disabled_filetypes = {
+--           statusline = {},
+--           winbar = {},
+--         },
+--         ignore_focus = {},
+--         always_divide_middle = true,
+--         always_show_tabline = true,
+--         refresh = {
+--           statusline = 100,
+--           tabline = 100,
+--           winbar = 100,
+--         }
+--       },
+--       sections = {
+--         lualine_a = { mode },
+--         -- lualine_b = { { 'branch', icons_enabled = true, icon = '' } },
+--         lualine_b = { { 'branch', icons_enabled = true, icon = ' ' } },
+--         lualine_c = {
+--           {
+--             'filename',
+--             symbols = {
+--               modified = '',
+--             }
+--           }
+--         },
+--         lualine_x = { diagnostics, diff, { 'encoding', cond = hide_in_width }, { 'filetype', cond = hide_in_width } },
+--         lualine_y = { 'progress' },
+--         lualine_z = { 'location' },
+--       },
+--       inactive_sections = {
+--         lualine_a = {},
+--         lualine_b = {},
+--         lualine_c = { 'filename' },
+--         lualine_x = { 'location' },
+--         lualine_y = {},
+--         lualine_z = {}
+--       },
+--       tabline = {
+--         -- extensions = { 'vim-fugitive' },
+--       },
+--       winbar = {},
+--       inactive_winbar = {},
+--       extensions = {}
+--     }
+--   end
+-- }
