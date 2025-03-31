a926cfabbb426d0af443141db4fa3323c8278be7
Sumir Vats <sumirvats@gmail.com>
2025-03-31 11:42:00 +0000

    remove inbuilt autocomplete issue

--- lua/plugins/dev/lsp-config.lua
+++ lua/plugins/dev/lsp-config.lua
@@ -60,10 +60,10 @@ return {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('UserLspConfig', {}),
         callback = function(ev)
-          local client = vim.lsp.get_client_by_id(ev.data.client_id)
-          if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-          end
+          -- local client = vim.lsp.get_client_by_id(ev.data.client_id)
+          -- if client:supports_method('textDocument/completion') then
+          --   vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+          -- end
           -- Enable completion triggered by <c-x><c-o>
           vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
