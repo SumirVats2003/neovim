e13fa5ee5075ed0286f69d30fb8198cd6b55898a
Sumir Vats <sumirvats@gmail.com>
2025-07-01 00:54:27 +0000

    removed faulty java plugin specs

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,139 +1,139 @@
-local config = {
-  cmd = {
-    'java',
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xms1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-    '-jar', vim.fn.glob('/home/' .. vim.fn.expand('$USER') .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
-    '-configuration', '/home/' .. vim.fn.expand('$USER') .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
-    '-data', vim.fn.expand('~/.cache/jdtls-workspace') .. vim.fn.getcwd(),
-  },
-
-  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle'}),
-
-  settings = {
-    java = {
-      eclipse = {
-        downloadSources = true,
-      },
-      configuration = {
-        updateBuildConfiguration = "interactive",
-        runtimes = {
-          -- Add your Java runtimes here if you have multiple versions
-          -- {
-          --   name = "JavaSE-17",
-          --   path = "/usr/lib/jvm/java-17-openjdk/",
-          -- },
-        }
-      },
-      maven = {
-        downloadSources = true,
-      },
-      implementationsCodeLens = {
-        enabled = true,
-      },
-      referencesCodeLens = {
-        enabled = true,
-      },
-      references = {
-        includeDecompiledSources = true,
-      },
-      format = {
-        enabled = true,
-        settings = {
-          url = vim.fn.stdpath "config" .. "/lang-servers/intellij-java-google-style.xml",
-          profile = "GoogleStyle",
-        },
-      },
-    },
-    signatureHelp = { enabled = true },
-    completion = {
-      favoriteStaticMembers = {
-        "org.hamcrest.MatcherAssert.assertThat",
-        "org.hamcrest.Matchers.*",
-        "org.hamcrest.CoreMatchers.*",
-        "org.junit.jupiter.api.Assertions.*",
-        "java.util.Objects.requireNonNull",
-        "java.util.Objects.requireNonNullElse",
-        "org.mockito.Mockito.*",
-      },
-      importOrder = {
-        "java",
-        "javax",
-        "com",
-        "org"
-      },
-    },
-    extendedClientCapabilities = require('jdtls').extendedClientCapabilities,
-    sources = {
-      organizeImports = {
-        starThreshold = 9999,
-        staticStarThreshold = 9999,
-      },
-    },
-    codeGeneration = {
-      toString = {
-        template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
-      },
-      useBlocks = true,
-    },
-  },
-
-  flags = {
-    allow_incremental_sync = true,
-  },
-
-  init_options = {
-    bundles = {}
-  },
-
-  on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings
-    local opts = { noremap=true, silent=true, buffer=bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
-
-    -- Java specific mappings
-    vim.keymap.set('n', '<leader>jo', "<Cmd>lua require'jdtls'.organize_imports()<CR>", opts)
-    vim.keymap.set('n', '<leader>jv', "<Cmd>lua require('jdtls').extract_variable()<CR>", opts)
-    vim.keymap.set('v', '<leader>jv', "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
-    vim.keymap.set('n', '<leader>jc', "<Cmd>lua require('jdtls').extract_constant()<CR>", opts)
-    vim.keymap.set('v', '<leader>jc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", opts)
-    vim.keymap.set('v', '<leader>jm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
-
-    -- Auto-format on save
-    if client.resolved_capabilities.document_formatting then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("LspFormatting", {}),
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ async = false })
-        end,
-      })
-    end
-  end,
-}
-
-require('jdtls').start_or_attach(config)
+-- local config = {
+--   cmd = {
+--     'java',
+--     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+--     '-Dosgi.bundles.defaultStartLevel=4',
+--     '-Declipse.product=org.eclipse.jdt.ls.core.product',
+--     '-Dlog.protocol=true',
+--     '-Dlog.level=ALL',
+--     '-Xms1g',
+--     '--add-modules=ALL-SYSTEM',
+--     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+--     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+--     '-jar', vim.fn.glob('/home/' .. vim.fn.expand('$USER') .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+--     '-configuration', '/home/' .. vim.fn.expand('$USER') .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
+--     '-data', vim.fn.expand('~/.cache/jdtls-workspace') .. vim.fn.getcwd(),
+--   },
+--
+--   root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle'}),
+--
+--   settings = {
+--     java = {
+--       eclipse = {
+--         downloadSources = true,
+--       },
+--       configuration = {
+--         updateBuildConfiguration = "interactive",
+--         runtimes = {
+--           -- Add your Java runtimes here if you have multiple versions
+--           -- {
+--           --   name = "JavaSE-17",
+--           --   path = "/usr/lib/jvm/java-17-openjdk/",
+--           -- },
+--         }
+--       },
+--       maven = {
+--         downloadSources = true,
+--       },
+--       implementationsCodeLens = {
+--         enabled = true,
+--       },
+--       referencesCodeLens = {
+--         enabled = true,
+--       },
+--       references = {
+--         includeDecompiledSources = true,
+--       },
+--       format = {
+--         enabled = true,
+--         settings = {
+--           url = vim.fn.stdpath "config" .. "/lang-servers/intellij-java-google-style.xml",
+--           profile = "GoogleStyle",
+--         },
+--       },
+--     },
+--     signatureHelp = { enabled = true },
+--     completion = {
+--       favoriteStaticMembers = {
+--         "org.hamcrest.MatcherAssert.assertThat",
+--         "org.hamcrest.Matchers.*",
+--         "org.hamcrest.CoreMatchers.*",
+--         "org.junit.jupiter.api.Assertions.*",
+--         "java.util.Objects.requireNonNull",
+--         "java.util.Objects.requireNonNullElse",
+--         "org.mockito.Mockito.*",
+--       },
+--       importOrder = {
+--         "java",
+--         "javax",
+--         "com",
+--         "org"
+--       },
+--     },
+--     extendedClientCapabilities = require('jdtls').extendedClientCapabilities,
+--     sources = {
+--       organizeImports = {
+--         starThreshold = 9999,
+--         staticStarThreshold = 9999,
+--       },
+--     },
+--     codeGeneration = {
+--       toString = {
+--         template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+--       },
+--       useBlocks = true,
+--     },
+--   },
+--
+--   flags = {
+--     allow_incremental_sync = true,
+--   },
+--
+--   init_options = {
+--     bundles = {}
+--   },
+--
+--   on_attach = function(client, bufnr)
+--     -- Enable completion triggered by <c-x><c-o>
+--     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+--
+--     -- Mappings
+--     local opts = { noremap=true, silent=true, buffer=bufnr }
+--     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+--     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+--     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+--     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+--     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+--     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+--     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+--     vim.keymap.set('n', '<space>wl', function()
+--       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--     end, opts)
+--     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+--     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+--     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+--     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+--     vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
+--
+--     -- Java specific mappings
+--     vim.keymap.set('n', '<leader>jo', "<Cmd>lua require'jdtls'.organize_imports()<CR>", opts)
+--     vim.keymap.set('n', '<leader>jv', "<Cmd>lua require('jdtls').extract_variable()<CR>", opts)
+--     vim.keymap.set('v', '<leader>jv', "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
+--     vim.keymap.set('n', '<leader>jc', "<Cmd>lua require('jdtls').extract_constant()<CR>", opts)
+--     vim.keymap.set('v', '<leader>jc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", opts)
+--     vim.keymap.set('v', '<leader>jm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
+--
+--     -- Auto-format on save
+--     if client.resolved_capabilities.document_formatting then
+--       vim.api.nvim_create_autocmd("BufWritePre", {
+--         group = vim.api.nvim_create_augroup("LspFormatting", {}),
+--         buffer = bufnr,
+--         callback = function()
+--           vim.lsp.buf.format({ async = false })
+--         end,
+--       })
+--     end
+--   end,
+-- }
+--
+-- require('jdtls').start_or_attach(config)
--- lua/plugins/tools/nvim-java.lua
+++ lua/plugins/tools/nvim-java.lua
@@ -1,7 +1,8 @@
-return {
-  'mfussenegger/nvim-jdtls',
-  ft = 'java',
-  config = function()
-    -- This will be configured in ftplugin/java.lua
-  end
-}
+return {}
+-- return {
+--   'mfussenegger/nvim-jdtls',
+--   ft = 'java',
+--   config = function()
+--     -- This will be configured in ftplugin/java.lua
+--   end
+-- }
