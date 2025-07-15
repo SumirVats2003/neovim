e69514b9d82d3afb42720f2e1e5f7e11b1bb476b
Sumir Vats <sumirvats@gmail.com>
2025-07-15 01:54:14 +0000

    uniformed icons

--- lua/plugins/dev/lsp-config.lua
+++ lua/plugins/dev/lsp-config.lua
@@ -36,10 +36,10 @@ return {
         severity_sort = true,
         signs = {
           text = {
-            [vim.diagnostic.severity.ERROR] = '',
-            [vim.diagnostic.severity.WARN] = '',
-            [vim.diagnostic.severity.INFO] = '',
-            [vim.diagnostic.severity.HINT] = '',
+            [vim.diagnostic.severity.ERROR] = ' ',
+            [vim.diagnostic.severity.WARN] = ' ',
+            [vim.diagnostic.severity.INFO] = ' ',
+            [vim.diagnostic.severity.HINT] = '󰛨 ',
           },
         },
       })
