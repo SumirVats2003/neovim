3f1c435453fed1b593a74e816f9d8d492e9785be
Sumir Vats <sumirvats@gmail.com>
2026-02-03 21:30:45 +0530

    added formatting options

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,3 +1,14 @@
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
+vim.keymap.set('n', '<leader>f', function()
+  vim.lsp.buf.format {
+    async = true,
+    formatting_options = {
+      tabSize = 4,
+      insertSpaces = true,
+      trimTrailingWhitespace = true,
+      insertFinalNewline = true
+    }
+  }
+end)
